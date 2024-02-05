a39f6feaa89e1a8d150547cf7df90e952b125a04
Your Name <your-email-address@domain.com>
2024-02-05 13:22:27 +0300

    firs stable version

--- fhttp.cpp
+++ fhttp.cpp
@@ -3,17 +3,40 @@
 #include<sys/socket.h> 
 #include<unistd.h> 
 #include<queue>
+#include<sstream>
+#include<iostream>
 
 
-fhttp::Server::Server(int port){
+std::string parse_url(std::string request){
+    int counter = 0;
+    std::stringstream ans;
+    for (auto i:request){
+        if (counter == 1 && i != ' '){
+            ans << i;
+        }
+        if(i == ' '){
+            counter += 1;
+            if (counter == 2){
+                break;
+            }
+        }
+    }
+    return ans.str().c_str();
+}
+
+
+void fhttp::Server::run(int port){
+    // creating socket 
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0); 
     int buffer_size;
-    std::queue<char> queue;
+    
+    std::string url;
+    std::string response;
 
     // specifying the address 
     sockaddr_in serverAddress; 
     serverAddress.sin_family = AF_INET; 
-    serverAddress.sin_port = htons(8080); 
+    serverAddress.sin_port = htons(port); 
     serverAddress.sin_addr.s_addr = INADDR_ANY; 
 
     // binding socket. 
@@ -22,25 +45,28 @@ fhttp::Server::Server(int port){
 
     // listening to the assigned socket 
     listen(serverSocket, 5); 
-
-    // accepting connection request 
+    char buffer[PACKET_SIZE] = { 0 }; 
     while (true){
         int clientSocket 
             = accept(serverSocket, nullptr, nullptr); 
-
-        char buffer[1024] = { 0 }; 
-        while (true){
-            buffer_size = recv(clientSocket, buffer, sizeof(buffer), 0); 
-            for (int i = 0; i < buffer_size; i++){
-                queue.push(buffer[i]);
-            }
-            if (buffer_size <= 0) break;
+        buffer_size = recv(clientSocket, buffer, sizeof(buffer) / sizeof(char), 0);
+        url = parse_url(buffer);
+        if (responce_functions.count(url)){
+            std::cout << "request to" << url << std::endl;
+            response = responce_functions[url](buffer);
+            send(clientSocket, response.c_str(), response.size(), 0);
         }
-        while (!queue.empty()){
-            queue.pop();
+        else{
+            std::cout << "invalid request to" << url << std::endl;
+            send(clientSocket, NOT_FOUND_RESPONSE, 25, 0);
         }
         close(clientSocket);
     }
     // closing the socket. 
-    close(serverSocket); 
+    close(serverSocket);
+}
+
+
+void fhttp::Server::request_hendler(std::string url, std::string (*hendler)(std::string)){
+    responce_functions[url] = hendler;
 }
--- fhttp.h
+++ fhttp.h
@@ -1,14 +1,17 @@
-#include<string> 
+#include<string>
+#include <map>
+#define OK_RESPONSE "HTTP/1.1 200 OK\r\n"
+#define NOT_FOUND_RESPONSE "HTTP/1.1 404 NOT_FOUND\r\n"
+#define PACKET_SIZE 4048
 
 
 namespace fhttp{
     class Server
     {
         private:
-            /* data */
+            std::map<std::string, std::string(*)(std::string)> responce_functions;
         public:
-            void get(std::string url, std::string (*obr)());
-            void post(std::string url, std::string (*obr)());
-            Server(int port);
+            void request_hendler(std::string url, std::string (*obr)(std::string));
+            void run(int port);
     };
 }
