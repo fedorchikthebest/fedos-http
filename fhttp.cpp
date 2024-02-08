9d69260538a15c0383f493d2b0d8a0c163eb0237
Fedor <fedorgorlenko@gmail.com>
2024-02-08 21:48:54 +0300

    added multi threading

--- fhttp.cpp
+++ fhttp.cpp
@@ -2,10 +2,18 @@
 #include<netinet/in.h> 
 #include<sys/socket.h> 
 #include<unistd.h> 
-#include<queue>
 #include<sstream>
 #include<iostream>
+#include<thread>
+#include<mutex>
 
+std::mutex mutex;
+std::queue<int> tasks;
+bool run = true;
+
+std::map<std::string, int> commands = {
+    std::pair
+};
 
 std::string parse_url(std::string request){
     int counter = 0;
@@ -25,13 +33,11 @@ std::string parse_url(std::string request){
 }
 
 
-void fhttp::Server::run(int port){
+void fhttp::Server::requests_accept_thread(){
     // creating socket 
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0); 
-    int buffer_size;
-    
-    std::string url;
-    std::string response;
+    int clientSocket;
+    int status;
 
     // specifying the address 
     sockaddr_in serverAddress; 
@@ -40,33 +46,78 @@ void fhttp::Server::run(int port){
     serverAddress.sin_addr.s_addr = INADDR_ANY; 
 
     // binding socket. 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, 
-        sizeof(serverAddress)); 
+    status = bind(serverSocket, (struct sockaddr*)&serverAddress, 
+        sizeof(serverAddress));
 
     // listening to the assigned socket 
     listen(serverSocket, 5); 
-    char buffer[PACKET_SIZE] = { 0 }; 
-    while (true){
-        int clientSocket 
+    while (run){
+        clientSocket 
             = accept(serverSocket, nullptr, nullptr); 
-        buffer_size = recv(clientSocket, buffer, sizeof(buffer) / sizeof(char), 0);
-        url = parse_url(buffer);
-        if (responce_functions.count(url)){
-            std::cout << "request to" << url << std::endl;
-            response = responce_functions[url](buffer);
-            send(clientSocket, response.c_str(), response.size(), 0);
-        }
-        else{
-            std::cout << "invalid request to" << url << std::endl;
-            send(clientSocket, NOT_FOUND_RESPONSE, 25, 0);
-        }
-        close(clientSocket);
+        std::cout << 2 << std::endl;
+        mutex.lock();
+        tasks.push(clientSocket);
+        std::cout << tasks.front() << std::endl;
+        mutex.unlock();
     }
     // closing the socket. 
     close(serverSocket);
 }
 
 
-void fhttp::Server::request_hendler(std::string url, std::string (*hendler)(std::string)){
+void fhttp::Server::requests_handler_thread(){
+    int clientSocket;
+    char buffer[PACKET_SIZE] = { 0 }; 
+    std::string url;
+    std::string response;
+    int buffer_size;
+
+    while (run){
+        mutex.lock();
+        if (!tasks.empty()){
+            clientSocket = tasks.front();
+            tasks.pop();
+            mutex.unlock();
+            buffer_size = recv(clientSocket, buffer, sizeof(buffer) / sizeof(char), 0);
+            url = parse_url(buffer);
+
+            if (responce_functions.count(url)){
+                std::cout << "request to " << url << std::endl;
+                response = responce_functions[url](buffer);
+                send(clientSocket, response.c_str(), response.size(), 0);
+            }
+            else{
+                std::cout << "invalid request to " << url << std::endl;
+                send(clientSocket, NOT_FOUND_RESPONSE, 25, 0);
+            }
+            close(clientSocket);
+            buffer[PACKET_SIZE] = { 0 };
+        }
+        mutex.unlock();
+    }
+}
+
+
+void fhttp::Server::run(){
+    std::thread requests_handler([](fhttp::Server srv){srv.requests_handler_thread();}, *this);
+    std::thread requests_accept([](fhttp::Server srv){srv.requests_accept_thread();}, *this);
+
+    requests_accept.join();
+    requests_handler.join();
+
+    std::string command;
+
+    while (true){
+
+    }
+}
+
+
+void fhttp::Server::request_handler(std::string url, std::string (*hendler)(std::string)){
     responce_functions[url] = hendler;
+}
+
+
+fhttp::Server::Server(int p){
+    port = p;
 }
--- fhttp.h
+++ fhttp.h
@@ -1,5 +1,6 @@
 #include<string>
 #include <map>
+#include<queue>
 #define OK_RESPONSE "HTTP/1.1 200 OK\r\n"
 #define NOT_FOUND_RESPONSE "HTTP/1.1 404 NOT_FOUND\r\n"
 #define PACKET_SIZE 4048
@@ -10,8 +11,12 @@ namespace fhttp{
     {
         private:
             std::map<std::string, std::string(*)(std::string)> responce_functions;
+            void requests_accept_thread();
+            void requests_handler_thread();
+            int port;
         public:
-            void request_hendler(std::string url, std::string (*obr)(std::string));
-            void run(int port);
+            void request_handler(std::string url, std::string (*obr)(std::string));
+            void run();
+            Server(int p);
     };
 }
